9bae7a2ae3b7b8978b39829ac850c454a14a4504
ogkunald <kunaldongare111@gmail.com>
2023-03-11 07:16:19 +0000

    okta authentication/authorization added

--- src/main/java/com/nastyzera/OrderService/controller/OrderController.java
+++ src/main/java/com/nastyzera/OrderService/controller/OrderController.java
@@ -3,12 +3,16 @@ package com.nastyzera.OrderService.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nastyzera.OrderService.model.OrderRequest;
+import com.nastyzera.OrderService.model.OrderResponse;
 import com.nastyzera.OrderService.service.OrderService;
 
 import lombok.extern.log4j.Log4j2;
@@ -21,7 +25,7 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
-   // @PreAuthorize("hasAuthority('Customer')")
+    @PreAuthorize("hasAuthority('Customer')")
     @PostMapping("/placeOrder")
     public ResponseEntity<Long> placeOrder(@RequestBody OrderRequest orderRequest) {
         // save data with order created
@@ -34,13 +38,13 @@ public class OrderController {
         return new ResponseEntity<>(orderId, HttpStatus.OK);
     }
 
-    // @PreAuthorize("hasAuthority('Admin') || hasAuthority('Customer')")
-    // @GetMapping("/{orderId}")
-    // public ResponseEntity<OrderResponse> getOrderDetails(@PathVariable long orderId) {
-    //     OrderResponse orderResponse
-    //             = orderService.getOrderDetails(orderId);
+    @PreAuthorize("hasAuthority('Admin') || hasAuthority('Customer')")
+    @GetMapping("/{orderId}")
+    public ResponseEntity<OrderResponse> getOrderDetails(@PathVariable long orderId) {
+        OrderResponse orderResponse
+                = orderService.getOrderDetails(orderId);
 
-    //     return new ResponseEntity<>(orderResponse,
-    //             HttpStatus.OK);
-    // }
+        return new ResponseEntity<>(orderResponse,
+                HttpStatus.OK);
+    }
 }
